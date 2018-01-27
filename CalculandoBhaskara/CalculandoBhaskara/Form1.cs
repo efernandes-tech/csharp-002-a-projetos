64bec1af0b5eace437784f6c87136b3f2a19bb41
edersonlrf <edersonluis.rf@gmail.com>
2018-01-27 10:33:42 -0200

    A2 calculando bhaskara

---
+++ CalculandoBhaskara/CalculandoBhaskara/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculandoBhaskara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 2;
+            int b = 5;
+            int c = 2;
+
+            double delta;
+            double a1;
+            double a2;
+
+            delta = b * b - 4 * a * c;
+            a1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            a2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            MessageBox.Show("a1 = " + a1 + " a2 = " + a2);
+        }
+    }
+}
