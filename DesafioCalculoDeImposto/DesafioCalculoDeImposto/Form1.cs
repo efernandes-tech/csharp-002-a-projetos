081dc95f8067759da86c30e6f9141f1ef4e20404
edersonlrf <edersonluis.rf@gmail.com>
2018-02-03 11:04:43 -0200

    A3 calculo de imposto

---
+++ DesafioCalculoDeImposto/DesafioCalculoDeImposto/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioCalculoDeImposto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valorDaNotaFiscal = 2000.0;
+            double imposto;
+
+            if (valorDaNotaFiscal < 1000)
+            {
+                imposto = 0.02;
+            }
+            else if (valorDaNotaFiscal < 3000)
+            {
+                imposto = 0.025;
+            }
+            else if (valorDaNotaFiscal < 7000)
+            {
+                imposto = 0.028;
+            }
+            else
+            {
+                imposto = 0.03;
+            }
+
+            MessageBox.Show("Imposto a ser pago: " + imposto);
+        }
+    }
+}
