50571fe0d698d22dc19f3c97e0ab9eb43a88054b
edersonlrf <edersonluis.rf@gmail.com>
2018-02-03 10:39:43 -0200

    A3 votando

---
+++ DesafioVotando/DesafioVotando/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioVotando
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = 15;
+            bool brasileira = true;
+
+            if (idade >= 16 && brasileira == true)
+            {
+                MessageBox.Show("Você pode votar!");
+            }
+            else
+            {
+                MessageBox.Show("Você não pode votar!");
+            }
+        }
+    }
+}
