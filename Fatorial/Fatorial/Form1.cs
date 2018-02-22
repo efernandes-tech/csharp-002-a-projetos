03a0462c3bed9ec9f29881350a24f563885367da
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-02-22 08:49:28 -0300

    A4 fatorial

---
+++ Fatorial/Fatorial/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fatorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fatorial = 1;
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial = fatorial * n;
+                MessageBox.Show("fatorial(" + n + ") = " + fatorial);
+            }
+        }
+    }
+}
