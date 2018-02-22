8c9e097fdad94656806023494bf666cebcd5c380
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-02-22 08:43:47 -0300

    A4 soma de 1 a 1000

---
+++ SomaNumeros/SomaNumeros/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomaNumeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int soma = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                soma += i;
+            }
+            MessageBox.Show("A soma dos números de 1 até 1000 é: " + soma);
+        }
+    }
+}
