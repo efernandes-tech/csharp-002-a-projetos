d08456778801d9dc316e4a696cd03e423f10043c
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-03-01 08:53:08 -0300

    A7 extraindo comportamentos: metodos

---
+++ CaixaEletronico2/CaixaEletronico2/Conta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico2
+{
+    class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        public void Saca(double valor)
+        {
+            this.saldo -= valor;
+        }
+
+        public void Deposita(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void Transfere(double valor, Conta destino)
+        {
+            this.Saca(valor);
+            destino.Deposita(valor);
+        }
+    }
+}
---
+++ CaixaEletronico2/CaixaEletronico2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta guilherme = new Conta();
+            guilherme.numero = 1;
+            guilherme.titular = "Guilherme";
+            guilherme.saldo = 1500.0;
+
+            Conta mauricio = new Conta();
+            mauricio.numero = 2;
+            mauricio.titular = "Mauricio";
+            mauricio.saldo = 2500.0;
+
+            guilherme.Transfere(200.0, mauricio);
+
+            MessageBox.Show("Saldo Guilherme: " + guilherme.saldo);
+            MessageBox.Show("Saldo Maurício: " + mauricio.saldo);
+        }
+    }
+}
