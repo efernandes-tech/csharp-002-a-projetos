5fd5654a1c1197b465d73e79c6a304623bad45de
edersonlrf <edersonluis.rf@gmail.com>
2018-03-04 10:15:58 -0300

    A7 adicionando e testando comportamento da conta

---
+++ CaixaEletronico3/CaixaEletronico3/Conta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico3
+{
+    class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        public void Deposita(double valorASerDepositado)
+        {
+            if (valorASerDepositado > 0)
+            {
+                this.saldo += valorASerDepositado;
+            }
+        }
+
+        public void Saca(double valorASerSacado)
+        {
+            if (this.saldo >= valorASerSacado && valorASerSacado > 0)
+            {
+                this.saldo -= valorASerSacado;
+            }
+        }
+    }
+}
