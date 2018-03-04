0e262e7dffa10d0e815379cb7981da5137680847
edersonlrf <edersonluis.rf@gmail.com>
2018-03-04 10:39:09 -0300

    A9 sacando com limites

--- CaixaEletronico3/CaixaEletronico3/Conta.cs
+++ CaixaEletronico3/CaixaEletronico3/Conta.cs
@@ -21,11 +21,34 @@ namespace CaixaEletronico3
             }
         }
 
-        public void Saca(double valorASerSacado)
+        // Método Saca devolverá um bool indicando se a operação foi bem sucedida.
+        public bool Saca(double valorASerSacado)
         {
-            if (this.saldo >= valorASerSacado && valorASerSacado > 0)
+            if (valorASerSacado > this.saldo || valorASerSacado < 0)
             {
-                this.saldo -= valorASerSacado;
+                // Nesse caso não posso fazer o saque então devolverei false.
+                return false;
+            }
+            else
+            {
+                if (this.titular.EhMaiorDeIdade())
+                {
+                    this.saldo -= valorASerSacado;
+                    // Consegui sacar, então vou devolver true.
+                    return true;
+                }
+                else
+                {
+                    if (valorASerSacado <= 200)
+                    {
+                        this.saldo -= valorASerSacado;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
             }
         }
 
--- CaixaEletronico3/CaixaEletronico3/Form1.cs
+++ CaixaEletronico3/CaixaEletronico3/Form1.cs
@@ -28,5 +28,28 @@ namespace CaixaEletronico3
 
             MessageBox.Show(conta.titular.nome); // Mostra Victor no MessageBox.
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conta contaGuilherme = new Conta();
+            contaGuilherme.numero = 1;
+            contaGuilherme.saldo = 1500.0;
+
+            Cliente clienteGuilherme = new Cliente();
+            clienteGuilherme.nome = "Guilherme";
+            clienteGuilherme.idade = 18;
+
+            contaGuilherme.titular = clienteGuilherme;
+
+            bool sacou = contaGuilherme.Saca(300.0);//testando idade
+            if (sacou)
+            {
+                MessageBox.Show("Saldo da Conta do Guilherme após saque: " + contaGuilherme.saldo);
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível sacar da conta do Guilherme");
+            }
+        }
     }
 }
